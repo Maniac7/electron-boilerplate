7b89ee587e5715f96b5e8179f3971e1b6d00948d
Maniac7 <jbuchanan455@gmail.com>
2018-12-26 13:54:08 +0000

    Implemented DB generation from JSON file

--- src/helpers/map_index.js
+++ src/helpers/map_index.js
@@ -3,16 +3,23 @@
 import enmap from "enmap";
 import fs from "fs";
 
-fs.mkdir(__dirname + '/mapdb', function (err) {
-    if(err !== "EEXIST") console.log(err);
-});
+//create /mapdb and handle error if it already exists
+fs.mkdir(__dirname + '/mapdb', function (err) {if(err) console.log(err);});
 
+//create/load persistant enmap in ./mapdb
 const mapIndex = new enmap({name: "map-index", dataDir: __dirname + "/mapdb"});
 
+//and load the json file with the default index if the db is not up-to-date
+let mapIndexStatus = fs.readFileSync(__dirname + '\\mapindexstatus.json', function (err) {console.log(err);});
+let mapIndexStatusCur = JSON.parse(mapIndexStatus);
+// if (mapIndexStatusCur = );
+
+let mapIndexJSON = fs.readFileSync(__dirname + '\\mapindex.json', function (err) {console.log(err);});
+const defaultMapIndex = JSON.parse(mapIndexJSON);
+
+//when db is ready, load values from mapindex.json into it
 mapIndex.defer.then( () => {
-mapIndex.ensure("map-index", {
-    sectorIndex: "Eriador,Rhovanion,Gondor,Mordor"
-});
-console.log(mapIndex.get("map-index", "sectorIndex"));
+mapIndex.ensure("map-index", defaultMapIndex);
+console.log(mapIndex.get("map-index"));
 });
 
