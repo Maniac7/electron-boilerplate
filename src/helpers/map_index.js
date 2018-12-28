edda66bb1b82c7d4ceb81eb5a1475e1dd00bb232
Maniac7 <jbuchanan455@gmail.com>
2018-12-28 23:05:13 +0000

    Mostly implemented nav tree generation

--- src/helpers/map_index.js
+++ src/helpers/map_index.js
@@ -1,10 +1,46 @@
-//loads latest map index from mapindex.json
+//loads latest map index and metadata from mapindex.json
 
 import fs from 'fs';
 
-let mapIndexJSON = fs.readFileSync(__dirname + '\\mapindex.json', (err) => {console.log(err)});
+let mapIndexJSON = fs.readFileSync(__dirname + '\\mapIndex.json', (err) => {console.log(err)});
 const mapIndex = JSON.parse(mapIndexJSON);
 
-console.log(mapIndex);
+console.log(mapIndex.mapArr);
 
-//initialise object containing all map names
+ mapIndex.newTreeItem = (depth,iname,name,parent,slevel,srcb) => {
+
+    let treeItem = document.createElement("div");
+    let itemName = document.createTextNode(name);
+    let itemParent = document.getElementById(parent);
+
+//add name of item to the element and append item
+    treeItem.appendChild(itemName);
+    itemParent.appendChild(treeItem);
+
+//set item id for appending children **REQUIRES PARENTS CREATED BEFORE CHILDREN**
+    treeItem.id = iname;
+
+//set order according to starting level
+    treeItem.style.order = slevel;
+
+//add class for styling
+    if(depth == "top") {
+        treeItem.classList.add("topTreeItem");
+    } else if (depth == "sub") {
+        treeItem.classList.add("subTreeItem");
+    };
+    
+    console.log(srcb);
+};
+
+//loop through array of maps and create item for each
+mapIndex.genNavTree = () => {
+    for (let iname of mapIndex.mapArr) {
+        let curItem = mapIndex[iname];
+        let srcb = null;
+        if (curItem.hasOwnProperty(srcb)) {srcb = curItem.srcb};
+        mapIndex.newTreeItem(curItem.depth,iname,curItem.name,curItem.parent,curItem.slevel,srcb);
+    }
+};
+
+mapIndex.genNavTree();
